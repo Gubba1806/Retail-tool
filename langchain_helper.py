64443c6d99099652a5916b970b230639002526e8
Gubba1806 <123963182+Gubba1806@users.noreply.github.com>
2024-07-23 20:00:53 +0530

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -23,7 +23,7 @@ def get_few_shot_db_chain():
 
     db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",
                               sample_rows_in_table_info=3)
-    llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
+    llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1) #keep your own google api key
 
     embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
     to_vectorize = [" ".join(example.values()) for example in few_shots]
